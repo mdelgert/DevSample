51ad11d4aba215d55e0864a2b56253a25170336e
Matthew David Elgert <mdelgert@yahoo.com>
2023-11-15 14:50:32 -0500

    Add summary to program.cs

--- Program.cs
+++ Program.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 
-//Prior total elapsed time: 4237765 milliseconds. Optimized total elapsed time: 111801 milliseconds. Around 4100% performance increase.
-
 namespace DevSample
 {
+    /// <summary>
+    /// Represents the entry point of the program. Prior total elapsed time: 4237765 milliseconds. 
+    /// Optimized total elapsed time: 111801 milliseconds. Around 4100% performance increase.
+    /// </summary>
     internal abstract class Program
     {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
         private static void Main()
         {
+            // Invokes the SampleProcessor to perform work cycles.
             SampleProcessor.WorkCycles();
+
+            // Pauses the console to keep it open for observation.
             Console.Read();
         }
     }
